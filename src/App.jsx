c4628519ca6fbd57f1db28e9b52da8f0e989e160
CEDRIC SIA <cedricsiaaa@gmail.com>
2023-05-31 13:57:48 +0200

    coucou les props

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import "./PokemonCard";
 import "./App.css";
 import PokemonCard from "./PokemonCard";
 
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+const pokemon = pokemonList[0];
 function App() {
-  return (
-    <>
-      <div>
-        <PokemonCard />
-      </div>
-    </>
-  );
+  return <PokemonCard pokemon={pokemon} />;
 }
-
 export default App;
--- src/PokemonCard.jsx
+++ src/PokemonCard.jsx
@@ -1,23 +1,11 @@
-function PokemonCard() {
-  const pokemon = pokemonList[0];
+function PokemonCard(props) {
+  const pokemon = props.pokemon;
   //prettier-ignore
   return (
     <figure>
-      {pokemon.imgSrc === undefined ?<p>???</p> :<img src={pokemon.imgSrc} alt={pokemon.name} />}
+      {pokemon.imgSrc === undefined ? <p>???</p> : <img src={pokemon.imgSrc} alt={pokemon.name} />}
       <figcaption>{pokemon.name}</figcaption>
     </figure>
   );
 }
-
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
-
 export default PokemonCard;
